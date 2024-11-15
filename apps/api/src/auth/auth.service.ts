55a6e3d52fd4a88ce0d893363d25300e5e09b6dc
harioctav <aldiamahari16@gmail.com>
2024-11-16 02:19:42 +0700

    feat: (api) create local auth guard

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,6 +1,11 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
+import { verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -11,5 +16,25 @@ export class AuthService {
     if (user) throw new ConflictException('User already exists!');
 
     return this.userService.create(createUserDto);
+  }
+
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('User not found!');
+    const isPasswordMatched = verify(user.password, password);
+    if (!isPasswordMatched)
+      throw new UnauthorizedException('Invalid Credentials!');
+
+    return {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      gender: user.gender,
+      date_of_birth: user.date_of_birth,
+      phone: user.phone,
+      create_at: user.created_at,
+      updated_at: user.updated_at,
+    };
   }
 }
