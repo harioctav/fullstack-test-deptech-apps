33cb2f7f49a63262298114cbc83fc3171fe182a7
harioctav <aldiamahari16@gmail.com>
2024-11-16 11:54:26 +0700

    feat: (auth) protected APIs with JWT Strategy

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +24,11 @@ export class AuthController {
   @Post('signin')
   login(@Request() request) {
     return this.authService.login(request.user.id, request.user.first_name);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  getAll(@Request() request) {
+    return `Now you can access this protected API. This is User Id: ${request.user.id}`;
   }
 }
